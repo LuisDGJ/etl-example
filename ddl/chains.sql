4e2ac4cd2f1cb451be8cd4649650c8e3ed98053d
ludagoju <ldaniel.gonzalez.j@gmail.com>
2025-09-05 14:14:48 -0600

    adición de scripts DDL chains, stores, products, sellout

---
+++ ddl/chains.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS chains CASCADE;
+
+create table chains (
+	chain_id INT,
+	chain_name VARCHAR(50)
+);
+insert into chains (chain_id, chain_name) values (1, 'Aimbu');
+insert into chains (chain_id, chain_name) values (2, 'Rooxo');
+insert into chains (chain_id, chain_name) values (3, 'Oyondu');
+insert into chains (chain_id, chain_name) values (4, 'Tazzy');
+insert into chains (chain_id, chain_name) values (5, 'Edgeify');
+insert into chains (chain_id, chain_name) values (6, 'Lazzy');
+insert into chains (chain_id, chain_name) values (7, 'Bluezoom');
+insert into chains (chain_id, chain_name) values (8, 'Ailane');
+insert into chains (chain_id, chain_name) values (9, 'Feedspan');
+insert into chains (chain_id, chain_name) values (10, 'Photobean');
